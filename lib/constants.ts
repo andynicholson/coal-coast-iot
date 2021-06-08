cd3461931f5cae6a6e3c72997958580aeebf58cf
Andy Nicholson <intothemist@gmail.com>
2021-06-08 16:35:59 +1000

    navigation tweak

--- lib/constants.ts
+++ lib/constants.ts
@@ -33,7 +33,7 @@ export const COOKIE = 'cciot-user-id';
 // Remove process.env.NEXT_PUBLIC_... below and replace them with
 // strings containing your own privacy policy URL and copyright holder name
 export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
-export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
+export const COPYRIGHT_HOLDER = 'BRICKMAN';
 
 export const CODE_OF_CONDUCT =
   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
@@ -41,20 +41,8 @@ export const REPO = 'https://github.com/andynicholson/coal-coast-iot';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Stage A',
-    route: '/stage/a'
-  },
-  {
-    name: 'Stage C',
+    name: 'Stage BRICKMAN',
     route: '/stage/c'
-  },
-  {
-    name: 'Stage M',
-    route: '/stage/m'
-  },
-  {
-    name: 'Stage E',
-    route: '/stage/e'
   },
   {
     name: 'Schedule',
